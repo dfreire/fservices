b47833368e08fd2dece14ce08839bc01d9e54612
dfreire <dario.freire@gmail.com>
2015-11-27 22:35:07 +0000

    update

--- auth2/auth_test.go
+++ auth2/auth_test.go
@@ -2,23 +2,33 @@ package auth2
 
 import (
 	"database/sql"
-	"path/filepath"
 	"testing"
 
 	"github.com/BurntSushi/toml"
-	"github.com/dfreire/fservices/mailer"
+	mailermock "github.com/dfreire/fservices/mailer/mock"
 	"github.com/dfreire/fservices/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestSignup(t *testing.T) {
-	auth, db := createAuthService()
+	auth, _, mailerMock, db := createAuthService()
 	defer dropSchema(db)
 
+	// request := mailer.SendMailRequest{
+	// 	From:    cfg.FromEmail,
+	// 	To:      []string{"dario.freire@gmail.com"},
+	// 	Subject: cfg.ConfirmationEmail["en_US"].Subject,
+	// 	Body:    util.MustRenderTemplate(cfg.ConfirmationEmail[lang].Body, templateValues)
+	// }
+	// mailerMock.On("Send", request).Return(nil)
+	mailerMock.On("Send", mock.AnythingOfTypeArgument("mailer.SendMailRequest")).Return(nil)
 	assert.Nil(t, auth.Signup("myapp", "dario.freire@gmail.com", "123", "en_US"))
+	mailerMock.AssertNumberOfCalls(t, "Send", 1)
+	mailerMock.AssertExpectations(t)
 }
 
-func createAuthService() (Auth, *sql.DB) {
+func createAuthService() (Auth, AuthConfig, *mailermock.MailerMock, *sql.DB) {
 	var authConfig AuthConfig
 	_, err := toml.DecodeFile("auth_test.toml", &authConfig)
 	util.PanicIfNotNil(err)
@@ -28,12 +38,13 @@ func createAuthService() (Auth, *sql.DB) {
 	storePg, err := NewStorePg(db)
 	util.PanicIfNotNil(err)
 
-	var smtpConfig mailer.SmtpConfig
-	_, err = toml.DecodeFile(filepath.Join("..", "mailer", "mailer_test.secret.toml"), &smtpConfig)
-	util.PanicIfNotNil(err)
-	mailer := mailer.NewMailer(smtpConfig)
+	// var smtpConfig mailer.SmtpConfig
+	// _, err = toml.DecodeFile(filepath.Join("..", "mailer", "mailer_test.secret.toml"), &smtpConfig)
+	// util.PanicIfNotNil(err)
+	// mailer := mailer.NewMailer(smtpConfig)
+	mailer := new(mailermock.MailerMock)
 
-	return NewAuth(authConfig, storePg, mailer), db
+	return NewAuth(authConfig, storePg, mailer), authConfig, mailer, db
 }
 
 func dropSchema(db *sql.DB) {
