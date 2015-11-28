9d1771843fcb03a13431c337069e55f84766f4b1
dfreire <dario.freire@gmail.com>
2015-11-28 12:36:08 +0000

    update

--- auth/store.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import "errors"
-
-type Store interface {
-	CreateUser(appId, email, hashedPass, confirmationToken string) error
-}
-
-var ErrUserEmailExists = errors.New("This email is already being used.")
